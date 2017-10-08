45001a30201b30bdfdc0e8a75cd73bbb0e1acc5d
amoragues <amoragues@itba.edu.ar>
2017-10-08 17:33:24 -0300

    more test added, dao test to be finished

--- persistence/src/test/java/ar/edu/itba/paw/persistence/PlayerJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/PlayerJdbcDaoTest.java
@@ -6,6 +6,7 @@ import ar.edu.itba.paw.model.User;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -13,6 +14,9 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 import javax.sql.DataSource;
@@ -20,14 +24,32 @@ import javax.sql.DataSource;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:schema.sql")
 public class PlayerJdbcDaoTest {
 
-    @Mock
-    PlayerJdbcDao playerJdbcDao;
+    @Autowired
+    private DataSource ds;
 
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
+    @Autowired
+    private PlayerJdbcDao playerDao;
+    private JdbcTemplate jdbcTemplate;
 
 
+    @Before
+    public void setUp() {
+        jdbcTemplate = new JdbcTemplate(ds);
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "player");
+    }
+
+    @Test
+    public void testCreate() {
+        final Player player = playerDao.create("Jorge");
+        assertNotNull(player);
+        assertEquals(player, player.getName());
+        assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "player"));
+    }
 
 }
