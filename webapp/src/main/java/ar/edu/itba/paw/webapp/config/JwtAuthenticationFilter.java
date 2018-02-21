1867635a9cb9a67438f8b6401da8adf1350a03f0
adebrouvier <ariel.debrouvier@gmail.com>
2018-02-21 18:11:56 -0300

    Add json web token authentication

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package ar.edu.itba.paw.webapp.config;
+
+import ar.edu.itba.paw.webapp.controller.dto.UserDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+  private AuthenticationManager authenticationManager;
+
+  static final String SECRET = "puOPzfYy4SPhrT1DZFdJHR5y9wDI0dDnHt9tdutB8iTc3n0FpLhUxKxYWbHdVaJ8uc0ja2ttcy5uqGtEAb0paKK6svBfp9oCqrAGzy0FAciF0zzQgxM2r0iVy86cismf";
+  private static final long EXPIRATION_TIME = 864_000_000; // 10 days
+  static final String TOKEN_PREFIX = "Bearer ";
+  static final String HEADER_STRING = "Authorization";
+
+  public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest req,
+                                              HttpServletResponse res) throws AuthenticationException {
+    try {
+      //TODO: authentication dto?
+      UserDTO creds = new ObjectMapper()
+        .readValue(req.getInputStream(), UserDTO.class);
+
+      return authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+          creds.getUsername(),
+          creds.getPassword(),
+          new ArrayList<>())
+      );
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest req,
+                                          HttpServletResponse res,
+                                          FilterChain chain,
+                                          Authentication auth) throws IOException, ServletException {
+
+    String token = Jwts.builder()
+      .setSubject(((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername())
+      .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+      .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
+      .compact();
+    res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+  }
+}
--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -10,14 +10,12 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -29,50 +27,15 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.userDetailsService(userDetailsService)
-                .sessionManagement()
-                .invalidSessionUrl("/")
-                .and().authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/register").anonymous()
-                .antMatchers("/login").anonymous()
-                .antMatchers(HttpMethod.POST, "/registerUser").anonymous()
-                .antMatchers(HttpMethod.GET, "/tournament").authenticated()
-                .antMatchers(HttpMethod.GET, "/ranking").authenticated()
-                .antMatchers(HttpMethod.GET, "/tournament/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/ranking/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/match/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/create/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/update/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/remove/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/delete/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/swap/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/comment/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/reply/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/accept/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/reject/**").authenticated()
-                .antMatchers("/**").permitAll()
-                .and().formLogin().usernameParameter("j_username")
-                .passwordParameter("j_password")
-                .defaultSuccessUrl("/", false)
-                .loginPage("/login")
-                .and().rememberMe()
-                .rememberMeParameter("j_rememberme")
-                .userDetailsService(userDetailsService)
-                .key("Xk5JrZuGN5uwD9jPBeTD2GaPgGJ7Bp832svkugEkF6kS3cxqcOTxw892ZRg7bE7OPqC8XyJFphxKGxg70uKDnfbKp4CtGyz8lEbXbsoCtwEabtyqTgaYnsOGkui1ul7K")
-                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
-                .and().logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .and().exceptionHandling()
-                .accessDeniedPage("/403")
-                .and().csrf().disable();
-    }
-
-    @Override
-    public void configure(final WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers("/resources/css/**", "/resources/js/**", "/img/**", "/favicon.ico", "/403");
+        http.csrf().disable().authorizeRequests()
+          .antMatchers(HttpMethod.POST, "/login").permitAll()
+          .anyRequest().authenticated()
+          .and()
+          .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+          .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+          // this disables session creation on Spring Security
+          .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          .and().httpBasic().authenticationEntryPoint(new JwtAuthenticationEntryPoint());
     }
 
     @Bean
