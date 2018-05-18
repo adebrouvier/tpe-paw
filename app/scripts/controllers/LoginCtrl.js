d000a9cc0393d32c79a2348564f0422b2eab8a16
adebrouvier <ariel.debrouvier@gmail.com>
2018-05-18 18:48:21 -0300

    Add auth service

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -1,38 +1,14 @@
-define(['tpePaw', 'services/titleService'], function(tpePaw) {
+define(['tpePaw', 'services/titleService', 'services/authService'], function(tpePaw) {
 
     'use strict';
-    tpePaw.controller('LoginCtrl', function($scope, $http, $window, $location, $filter, titleService) {
+    tpePaw.controller('LoginCtrl', function($scope, $http, $window, $location, $filter, titleService, AuthService) {
 
         titleService.setTitle($filter('translate')("LOGIN_TITLE") + " - Versus");
 
     	$scope.loginForm = {};
 
-    	$scope.login = function () {
-
-            if ($scope.loginForm.$valid){
-                $http.post('http://localhost:8080/login', $scope.loginForm)
-                        .then(function successCallback(response) {
-
-                            if (response.headers('Authorization')) {
-
-                                var currentUser = {
-                                    username: $scope.loginForm.username,
-                                    token: response.headers('Authorization')
-                                };
-
-                                if ($scope.loginForm.rememberMe){
-                                    $window.localStorage['currentUser'] = angular.toJson(currentUser);
-                                }else{
-                                    $window.sessionStorage['currentUser'] = angular.toJson(currentUser);
-                                }
-                                $scope.loggedIn = true;
-                                $location.path('/');
-                                console.log('loggedIn');
-                            }
-                        }, function errorCallback(response) {
-                            console.log('Login ERROR');
-                });
-            }
+        $scope.login = function () {
+            AuthService.login($scope.loginForm);
         };
     });
 });
